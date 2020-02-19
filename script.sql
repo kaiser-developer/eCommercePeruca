7f83aca85499d2325e8fb82308c33bc0e259b516
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-19 15:48:12 -0300

    branch gabriel criada

--- script.sql
+++ script.sql
@@ -8,15 +8,6 @@ CREATE TABLE `clientes` (
   `email` varchar(70) NOT NULL,
   `senha` varchar(45) NOT NULL,
   `sexo` varchar(12) NOT NULL,
-  `endereco` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
-CREATE TABLE `loja_peruca`.`funcionarios` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cargo` VARCHAR(100) NOT NULL,
-  `registro` VARCHAR(45) NOT NULL,
-  `nome` VARCHAR(50) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`)
-  )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
