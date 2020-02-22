80d318a43fd480c27105c3567beccd2500b7a3d2
michelle-santana <michelle.raiadrogasil@gmail.com>
2020-02-22 15:28:19 -0300

    Salvando pág cadastre-se

--- screens/js/main.js
+++ screens/js/main.js
@@ -1,1 +1,16 @@
-let inputSenha = document.getElementById('password');
+ let inputSenha = document.getElementById('Senha');
+ let inputConfirm = document.getElementById('confirmeSenha')
+
+inputConfirm.addEventListener("keyup", (e) => {
+    if (inputConfirm.value != inputSenha.value) {
+         inputConfirm.setAttribute("class", "form-control is-invalid")
+
+ } else {
+         inputConfirm.setAttribute("class", "form-control is-valid")
+     }
+
+ })
+
+
+
+
