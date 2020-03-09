39417fa99b53b3664192ebd56657c52909c8866e
Treinamento <Treinamento@RD723926.raiadrogasil.com>
2020-03-09 14:02:47 -0300

    Projeto atualizado

---
+++ src/app/components/produtos-geral/produtos-geral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-geral',
+  templateUrl: './produtos-geral.component.html',
+  styleUrls: ['./produtos-geral.component.css']
+})
+export class ProdutosGeralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
