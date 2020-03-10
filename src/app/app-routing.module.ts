50eae8ae147b7fb7efcfb403b8762b3aa71cf74a
Gabriel Maziero <gabrielmaziero@gmail.com>
2020-03-10 14:35:19 -0300

    adicionando um pull da DV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from './components/login/login.component'
 import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { ContatoComponent } from "./components/contato/contato.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'checkout', pathMatch: 'full'},
   { path: 'checkout', component: CheckoutComponent},
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'contato', component: ContatoComponent }
 ];
 
 @NgModule({
