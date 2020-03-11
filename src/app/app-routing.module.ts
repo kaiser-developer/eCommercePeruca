06b44666eff6158d8b74bcff0a7d8b6f4d624938
Treinamento <Treinamento@RD723926.raiadrogasil.com>
2020-03-11 15:31:31 -0300

    Rotas atualizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './components/login/login.component'
 import { CheckoutComponent } from "./components/checkout/checkout.component";
 import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
 import { PaginaProdutoComponent } from './components/pagina-produto/pagina-produto.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'checkout', pathMatch: 'full'},
   { path: 'checkout', component: CheckoutComponent},
   { path: 'login', component: LoginComponent },
+  { path: 'cadastre', component: CadastroComponent },
   { path: 'quemSomos', component: QuemSomosComponent },
   { path: 'paginaProduto', component: PaginaProdutoComponent }
 
