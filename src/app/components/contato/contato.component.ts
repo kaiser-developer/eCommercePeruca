a564ea249aaa8bf009d57432333aef961a1292c6
Daniel Rodrigues <daniroddra1999@gmail.com>
2020-03-19 12:27:01 -0300

    Pagina de Contato com validações e reset ok

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validacoes } from 'src/app/model/validacoes';
+import { ReactiveFormsModule } from "@angular/forms";
+import  {  FormBuilder,  FormGroup  }  from  '@angular/forms';
+import { Contato } from 'src/app/model/contato';
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+  formContato: FormGroup;
+  validacoes: Validacoes = new Validacoes;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.createForm(new Contato());
+   }
+   createForm(contato: Contato){
+     this.formContato = this.formBuilder.group({
+       nome: [contato.nome],
+       telefone: [contato.telefone],
+       email: [contato.email],
+       msg: [contato.msg]
+     })
+   }
+    onSubmit(){
+      this.createForm(new Contato());
+    }
+
+    permitirLetrasCont(evento: any) {
+      this.validacoes.cancelarNumeros(evento);
+    }
+    permitirNumerosCont(evento: any){
+      this.validacoes.cancelarLetras(evento);
+    }  
 }
