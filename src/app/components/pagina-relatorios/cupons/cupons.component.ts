1968578351dc059193626fb250bcfa0a2fce9f10
vtoliv <vitorturmac@gmail.com>
2020-04-13 16:32:40 -0300

    botoes de ativar e desativar cupom

--- src/app/components/pagina-relatorios/cupons/cupons.component.ts
+++ src/app/components/pagina-relatorios/cupons/cupons.component.ts
@@ -10,16 +10,36 @@ import { RequisicoesService } from 'src/app/services/requisicoes.service';
 export class CuponsComponent implements OnInit {
   
   cupons: Cupom[] = [];
+  
+  Desativar: false
+  
 
   constructor(private requisicoes: RequisicoesService) { 
-  this.requisicoes.getCupons().subscribe(
-    data => {
-      this.cupons = data;
-    }
-  )
-}
-
-  ngOnInit(): void {
+    this.requisicoes.getCupons().subscribe(
+      data => {
+        this.cupons = data;
+      }
+    )
   }
+
+  ativarCupom() {
+    let ativar = true;
+    for(let i = 0;i < this.cupons.length;i++) {
+      if(this.cupons[i].ativo == false) {
+        return this.cupons[i].ativo == ativar;
+      }
+    }
+  }
+
+  desativarCupom() {
+    let desativar = false;
+    for(let i = 0;i < this.cupons.length;i++) {
+      if(this.cupons[i].ativo == true) {
+        return this.cupons[i].ativo = desativar;
+      }
+    }
+  }
+
+  ngOnInit(){}
 
 }
